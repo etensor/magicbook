ae93320eb22f8940d8ae0576f16fdad8c336341c
etensor <dpenillac@correo.usbcali.edu.co>
2022-10-10 14:57:55 -0500

    models migration -> estructura definida

--- djangoApp/magicbook/models.py
+++ djangoApp/magicbook/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False)
+    username = models.CharField(max_length=50,verbose_name='Username')
+    email = models.EmailField()
+    password = models.CharField(max_length=50,verbose_name='Password')
+    api_keys = models.JSONField(default=dict, blank=True, null=True)
+
+
+
+    def __str__(self):
+        return f"{self.username} | ID: {self.id} , {self.email}"
+
+
+class Dream(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+    titulo = models.TextField(max_length=200)
+    is_public = models.BooleanField(default=False)
+    userId = models.ForeignKey(Usuario, on_delete=models.CASCADE) # BigAutoField -> ?
+    title = models.TextField(max_length=400)
+
+
+
+
+class AI_Answer(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False)
+    dreamId = models.ForeignKey(Dream, on_delete=models.CASCADE)
+    prompt = models.TextField(max_length=600)
+    ans = models.JSONField(default=dict, blank=True, null=True)
+    params = models.JSONField(default=dict, blank=True, null=True)
+
+
+

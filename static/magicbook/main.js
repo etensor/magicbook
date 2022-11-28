7b5be4c5ba78c00280852ffd8284e78162c92e7a
etensor <dpenillac@correo.usbcali.edu.co>
2022-11-28 06:42:41 -0500

    homemade dark mode css, musiquita upgrades | hue shift javascript idea -> future filters free

--- static/magicbook/main.js
+++ static/magicbook/main.js
@@ -23,4 +23,29 @@ by Ching Yeh and Greg Rutkowski, detailed cyberpunk
 in the style of GitS 1995`
 }
 
+
+
+function activarDarkMode(){
+    //var darkButton = document.getElementById('darkBtn');
+    if !DarkReader.isEnabled(){
+        DarkReader.enable({contrast: 5})
+    } else {
+        DarkReader.disable();
+    }
+}
+
  */
+
+function activarDarkMode(){
+    const el = document.body
+    el.classList.toggle("dark-modo")
+    let images = document.querySelectorAll("img");
+
+    // haxito para revertir  invert global effect: A,B -> B,A ::
+    images.forEach((imagen) => {
+        //imagen.style.filter = "invert(100%) hue-rotate(0.5turn)";
+        imagen.classList.toggle("dark-image")
+        // a class containing that.
+    }) // -> hue shift can make up NICE effects. for sure
+
+}
